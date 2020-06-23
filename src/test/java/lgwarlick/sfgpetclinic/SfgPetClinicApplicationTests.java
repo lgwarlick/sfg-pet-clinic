912387485b73bde28184c6950296f4bd8e6a3ee8
Logan <lgwarlick@gmail.com>
2020-06-23 13:40:02 -0500

    Initial Commit

---
+++ src/test/java/lgwarlick/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package lgwarlick.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
