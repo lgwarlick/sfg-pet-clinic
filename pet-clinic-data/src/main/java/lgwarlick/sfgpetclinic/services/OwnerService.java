ad0ac8287ab09f48776662fb041f9b42aabd6084
Logan <lgwarlick@gmail.com>
2020-06-27 16:07:18 -0500

    refactoring services to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/lgwarlick/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/lgwarlick/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package lgwarlick.sfgpetclinic.services;
 
 import lgwarlick.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
 
-    Owner findByID(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
