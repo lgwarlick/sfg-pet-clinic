79b112513335a19e4e0d3b9582539fa0b19dbca0
Logan <lgwarlick@gmail.com>
2020-08-10 09:41:18 -0500

    Unit tests for OwnerMapService. Closes #49

--- pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/SfgPetClinicApplicationTests.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class SfgPetClinicApplicationTests {
 
     @Test
