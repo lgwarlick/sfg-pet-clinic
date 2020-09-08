ad94433c2c1103ae0eb156223da7ed0da27877a3
Logan <lgwarlick@gmail.com>
2020-09-08 17:08:45 -0500

    Updated OwnerController and added ability to display owner details

--- pet-clinic-web/src/main/java/lgwarlick/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/lgwarlick/sfgpetclinic/controllers/OwnerController.java
@@ -3,12 +3,16 @@ package lgwarlick.sfgpetclinic.controllers;
 import lgwarlick.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 //alternative method that refactors out the root of /owners
 //see VetController for contrast
 @RequestMapping("/owners")
-
 @Controller
 public class OwnerController {
 
@@ -16,6 +20,12 @@ public class OwnerController {
 
     public OwnerController(OwnerService ownerService) {
         this.ownerService = ownerService;
+    }
+
+    //prevents id field from being manipulated
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"", "/", "/index", "/index.html"})
@@ -27,6 +37,13 @@ public class OwnerController {
    @RequestMapping("/find")
    public String findOwners() {
         return "notimplemented";
+   }
+
+   @GetMapping("/{ownerId}")
+    public ModelAndView showOwner (@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
    }
 
 }
