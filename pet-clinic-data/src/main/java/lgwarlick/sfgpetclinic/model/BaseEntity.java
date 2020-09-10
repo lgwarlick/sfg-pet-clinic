528bd7b490450d18d73b873827199904617a7022
Logan <lgwarlick@gmail.com>
2020-09-10 15:56:42 -0500

    Added views for PetController and created unit test for PetController

--- pet-clinic-data/src/main/java/lgwarlick/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/lgwarlick/sfgpetclinic/model/BaseEntity.java
@@ -20,15 +20,15 @@ public class BaseEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public boolean isNew() {
+        return this.id == null;
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public boolean isNew() {
-        return this.id == null;
     }
 }
